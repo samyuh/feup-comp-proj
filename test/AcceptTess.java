278bad3cbdfe534f19de0c30ff6444d72422da79
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2021-03-10 21:59:28 +0000

    Error on file findMaximum

--- test/AcceptTess.java
+++ test/AcceptTess.java
@@ -10,17 +10,23 @@ import java.io.StringReader;
 import pt.up.fe.comp.TestUtils;
 import pt.up.fe.specs.util.SpecsIo;
 
-public class ExampleTest {
+public class AcceptTess {
     // Hello World, Simple
     @Test
     public void HelloWorld() {
         String jmmCode = SpecsIo.getResource("fixtures/public/HelloWorld.jmm");
         assertEquals("Program", TestUtils.parse(jmmCode).getRootNode().getKind());
     }
-    
+
     @Test
     public void Simple(){
         String jmmCode = SpecsIo.getResource("fixtures/public/Simple.jmm");
+        assertEquals("Program", TestUtils.parse(jmmCode).getRootNode().getKind());
+    }
+
+    @Test
+    public void FindMaximum(){
+        String jmmCode = SpecsIo.getResource("fixtures/public/FindMaximum.jmm");
         assertEquals("Program", TestUtils.parse(jmmCode).getRootNode().getKind());
     }
 
