b62fba93bd3cc0707b0ba8bdfcf4ca8845ab86ec
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2021-05-29 21:42:21 +0100

    Populating the def

--- src/RegisterAllocation/DataflowAnalysis.java
+++ src/RegisterAllocation/DataflowAnalysis.java
@@ -1,8 +1,6 @@
 package RegisterAllocation;
 
-import org.specs.comp.ollir.Method;
-import org.specs.comp.ollir.Node;
-import org.specs.comp.ollir.NodeType;
+import org.specs.comp.ollir.*;
 
 import java.util.ArrayList;
 
@@ -12,23 +10,61 @@ import java.util.ArrayList;
  */
 public class DataflowAnalysis {
     Method method;
-    ArrayList<ArrayList<String>> use;
+    String[][] use;
+    String[][] def;
 
-    public DataflowAnalysis(Method method){
+    public DataflowAnalysis(Method method) {
         this.method = method;
         this.method.buildCFG();
-        this.use = new ArrayList<>();
+        this.use = new String[method.getInstructions().size()][];
+        this.def = new String[method.getInstructions().size()][];
     }
 
-    public void build(){
-        buildUse();
+    public void build() {
+        buildUseDef();
     }
 
-    public void buildUse(){
+    public void buildUseDef() {
         Node currentInst = method.getBeginNode();
-        while(currentInst.getNodeType() != NodeType.END){
-            currentInst.showNode();
+        do {
             currentInst = currentInst.getSucc1();
+            getUsedDefVariables(currentInst);
+        } while (currentInst.getSucc1().getNodeType() != NodeType.END);
+        printMatrix(def);
+    }
+
+    /**
+     * Stores in the array the used and defined variables.
+     */
+    public void getUsedDefVariables(Node node) {
+        int index = node.getId() - 1;
+        Instruction instruction = method.getInstr(index);
+
+        def[index] = new String[]{};
+        use[index] = new String[]{};
+
+        // Defined variable.
+        if (instruction.getInstType() == InstructionType.ASSIGN)
+            storeDefined((AssignInstruction) instruction, index);
+
+        // Used variable.
+
+    }
+
+    /**
+     * Stored the defined variable in the array structure.
+     */
+    public void storeDefined(AssignInstruction instruction, int index) {
+        Operand dest = (Operand) instruction.getDest();
+        def[index] = new String[]{dest.getName()};
+    }
+
+    public void printMatrix(String[][] mat) {
+        System.out.print("[");
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++)
+                System.out.print("[" + mat[i][j] + "]");
         }
+        System.out.print("]");
     }
 }
